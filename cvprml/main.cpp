c72a92ab34b9dcf167c7efec26074b54b845871d
wangying <yingwww@gmail.com>
2010-01-12 10:22:14 +0800

    main.cpp modified

--- cvprml/main.cpp
+++ cvprml/main.cpp
@@ -1,8 +1,10 @@
-#include <QtCore/QCoreApplication>
+#include <QtGui/QtGui>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
+    QPushButton button;
+    button.show();
     return a.exec();
 }
